a72d85c27519ebd8d271665105fcd8c6fd503f6e
Sean Bannigan <bannigan@meetmaestro.com>
2020-06-02 15:06:03 -0400

    sample app

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSearchbar } from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
 
-  constructor() {}
+    @ViewChild(IonSearchbar) searchbar: IonSearchbar;
+
+    constructor() { }
+
+    async scrollStart() {
+        const el = await this.searchbar.getInputElement();
+        el.blur();
+    }
 
 }
